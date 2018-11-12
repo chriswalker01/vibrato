1673a2d9b1da02d1a44fe5f697e2346fc752af7b
chriswalker01 <chris@walker.me>
2018-11-12 01:22:38 +0000

    Added some error checking and comments. Replacd hardcoded db connection settings with variables.

--- www/index.php
+++ www/index.php
@@ -10,18 +10,27 @@ New String: <input name="newstring" type="text" />
 
 <?php
 
+// DB variables
 $servername = "docker.walker.home";
 $database = "vibrato";
 $username = "vibratotest";
 $password = "MySuperSecureUserPassword";
 
-$mysqli = new mysqli('docker.walker.home', 'vibratotest', 'MySuperSecureUserPassword', 'vibrato');
+// Connect to database
+$mysqli = new mysqli("$servername", "$username", "$password", "$database");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to database" . "<br>";
+    exit;
+}
 
+// Get inputs from HTML section above
 $newstring = $_POST['newstring'];
+// Check to see if newstring has been set to anything.
 if (!empty($newstring)) {
+    // Add string to DB. Error if not successfull.
     $sql = "INSERT INTO mystrings (string) VALUES ('$newstring')";
     if (!$mysqli->query($sql) === TRUE) {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Failed to add string to DB." . "<br>";
     }
 }
 
